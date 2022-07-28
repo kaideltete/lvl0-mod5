483f0ba6be8f733601db9b1bebee990ac9cc8e1d
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-28 16:35:39 -0700

    Update _a_prime_or_not.py

--- _02_algorithms /_a_prime_or_not.py
+++ _02_algorithms /_a_prime_or_not.py
@@ -14,10 +14,8 @@ if __name__ == '__main__':
     #     is prime.
     #  3. If the number is divisible by any number other than 1 or itself,
     #     the number is not prime.
-
-
     q1 = simpledialog.askinteger(title='number', prompt="give me a number and i will tell you if it is prime")
-    cou=0
+    count=0
     for i in range(2, q1):
         ansc = (q1)%(i)
         print(ansc)
@@ -25,9 +23,9 @@ if __name__ == '__main__':
             messagebox.showinfo('',"that is not a prime number")
             break
         else:
-            cou += 1
-    print(cou)
-    if cou == q1-2:
+            count += 1
+    print(count)
+    if count == q1-2:
         messagebox.showinfo('',"that is a prime number")
 
 
