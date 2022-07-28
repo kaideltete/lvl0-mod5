d205033babc9a74cf125f3bb55f8f05ab3be7f37
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-28 13:12:52 -0700

    Update _d_happy_pet.py

--- _01_function_writing /_d_happy_pet.py
+++ _01_function_writing /_d_happy_pet.py
@@ -4,7 +4,6 @@ Write methods to represent the activities that will make the user's pet happy.
 from tkinter import messagebox, simpledialog, Tk
 
 
-if __name__ == '__main__':
     # TODO)
     #   1. Ask the user to enter the type of pet they want (give them a few
     #      choices).
@@ -15,4 +14,46 @@ if __name__ == '__main__':
     #      Each activity should increase (or decrease) the pet's happiness
     #      level by a different amount, depending on the kind of pet they
     #      have. For example, a fish might not enjoy a walk!
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+    u_score = 0
+    pet = simpledialog.askstring(title='pet', prompt="what pet do you want, cat or dog?")
+
+    q1 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at food bowl")
+    q2 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at door")
+    q3 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at toys")
+    q4 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at door")
+    q5 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at food bowl")
+    q6 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at toys")
+    q7 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at door")
+    q8 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at toys")
+    q9 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at food bowl")
+    q10 = simpledialog.askstring(title='feed, play, or walk?', prompt="looking at toys")
+
+    if (q1 == "feed"):
+        u_score += 1
+    if (q2 == "walk"):
+        u_score += 1
+    if (q3 == "play"):
+        u_score += 1
+    if (q4 == "walk"):
+        u_score += 1
+    if (q5 == "feed"):
+        u_score += 1
+    if (q6 == "play"):
+        u_score += 1
+    if (q7 == "walk"):
+        u_score += 1
+    if (q8 == "play"):
+        u_score += 1
+    if (q9 == "feed"):
+        u_score += 1
+    if (q10 == "play"):
+        u_score += 1
+
+    if u_score > 7:
+        messagebox.showinfo(message="you keept your " +pet+ " happy")
+    else:
+        messagebox.showinfo(message="you didn't keep your " + pet + " happy")
     pass
