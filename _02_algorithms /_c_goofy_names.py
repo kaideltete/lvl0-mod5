b89eccc7074bb7ffd5fc84a4bef2ce98d9acd750
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-29 16:00:25 -0700

    Update _c_goofy_names.py

--- _02_algorithms /_c_goofy_names.py
+++ _02_algorithms /_c_goofy_names.py
@@ -1,8 +1,9 @@
 """
 Write an algorithm to change a string into a "goofy" version.
 """
+import random
 from tkinter import messagebox, simpledialog, Tk
-
+window=Tk()
 
 if __name__ == '__main__':
     # TODO)
@@ -13,4 +14,18 @@ if __name__ == '__main__':
     #     For example, if they enter their name as Alexander Hamilton
     #     their goofy name will be AlExAnDeR HaMiLtOn
     #  3. Show the user the goofy version of their name in a pop-up.
+
+    name1 = simpledialog.askstring(title='1?', prompt="what is your name")
+
+    name2=""
+
+    for i in range(len(name1)):
+        var=random.randint(0, 1)
+        if var == 1:
+            name2=name2+name1[i:i+1].lower()
+        if var == 0:
+            name2=name2+name1[i:i+1].upper()
+
+
+    messagebox.showinfo(message="here is your changed name,"+str(name2)+".")
     pass
